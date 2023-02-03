723ff57004f0b23b674cd4935d71aa8a8b374d3e
Floo <fmanice07@gmail.com>
2023-02-03 15:28:41 +0100

    feat(obf): Recoded obfuscator in class

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -4,34 +4,78 @@ import tkinter.messagebox
 import os
 import subprocess
 
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("dark-blue")
 
-root = customtkinter.CTk()
-root.geometry("500x350")
-root.title("Python Obfuscator")
+class Obfuscator:
+    def __init__(self):
+        self.root = customtkinter.CTk()
+        self._filepath = ""
 
-frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=20, padx=60, fill="both", expand=True)
+        self.root.geometry("800x550")
+        self.root.title("Python Obfuscator")
 
+        self.frame = customtkinter.CTkFrame(master=self.root)
+        self.frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-def on_submit():
-    filepath = filedialog.askopenfilename(filetypes=[("Python Files", "*.py")])
+        self.file_path_label = customtkinter.CTkLabel(master=self.frame, text="Select the file you wanna obfuscate")
+        self.select_file_button = customtkinter.CTkButton(master=self.frame, text="Select File",
+                                                          command=self.on_select_file_button_submit)
+        self.obfuscate_button = customtkinter.CTkButton(master=self.frame, text="Obfuscate",
+                                                        command=self.on_obfuscate_button_submit)
 
-    if filepath == "":
+        self.construct_main_window()
+
+        self.root.mainloop()
+
+    @property
+    def filepath(self):
+        return self._filepath
+
+    @filepath.setter
+    def filepath(self, new_filepath):
+        self._filepath = new_filepath
+
+        if self.filepath == "":
+            self.file_path_label.configure(text="Select the file you wanna obfuscate")
+            return
+
+        self.file_path_label.configure(text=f"Selected file: {self.filepath}")
+
+    def construct_main_window(self):
+        self.file_path_label.pack(pady=(200, 10))
+        self.select_file_button.pack(pady=10)
+        self.obfuscate_button.pack(pady=10)
+
+    def destroy_main_window(self):
+        self.file_path_label.destroy()
+        self.select_file_button.destroy()
+        self.obfuscate_button.destroy()
+
+    def on_select_file_button_submit(self):
+        filepath = filedialog.askopenfilename(filetypes=[("Python Files", "*.py")])
+
+        if filepath == "":
+            return
+
+        self.filepath = filepath
+
+    def on_obfuscate_button_submit(self):
+        if self.filepath == "":
+            tkinter.messagebox.showerror("Python Obfuscator", "No file selected.")
+            return
+
+        directory = os.path.dirname(self.filepath)
+        name = os.path.basename(self.filepath)
+
+        subprocess.call(["pyarmor", "obfuscate", "--output=%s/obfuscated" % directory, self.filepath],
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.DEVNULL)
+
+        self.filepath = ""
+
+        tkinter.messagebox.showinfo("Python Obfuscator", f"The obfuscate of {name} was successful!")
+
         return
 
-    directory = os.path.dirname(filepath)
-    name = os.path.basename(filepath)
-    subprocess.call(["pyarmor", "obfuscate", "--output=%s/obfuscated" % directory, filepath], stdout=subprocess.DEVNULL,
-                    stderr=subprocess.DEVNULL)
-    tkinter.messagebox.showinfo("Python Obfuscator", f"The obfuscate of {name} was successful!")
 
-
-label = customtkinter.CTkLabel(master=frame, text="Select the file you wanna obfuscate")
-label.pack(pady=(100, 10))
-
-button1 = customtkinter.CTkButton(master=frame, text="Obfuscate", command=on_submit)
-button1.pack(pady=10)
-
-root.mainloop()
+if __name__ == "__main__":
+    obfuscator = Obfuscator()
